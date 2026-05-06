603616e0156d61746f9104741b9d09a4024588a2
smary17 <maryanabrobles@gmail.com>
2026-05-05 18:57:01 -0600

    Update server.js

--- server.js
+++ server.js
@@ -184,6 +184,8 @@ app.post('/auditoria', (req, res) => {
 /* =========================
    SERVIDOR
 ========================= */
-app.listen(3000, () => {
-    console.log("🚀 Servidor en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Servidor corriendo en puerto " + PORT);
 });
